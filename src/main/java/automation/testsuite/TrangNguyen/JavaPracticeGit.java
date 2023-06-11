1b9b4bd8278c304a2f53da27e76b2b78096ac26f
lilyhank1201 <135857381+lilyhank1201@users.noreply.github.com>
2023-06-11 20:46:07 +0700

    trangnth

---
+++ src/main/java/automation/testsuite/TrangNguyen/JavaPracticeGit.java
@@ -0,0 +1,13 @@
+package automation.testsuite.TrangNguyen;
+
+import java.util.Scanner;
+
+public class JavaPracticeGit {
+	public static void main(String[] args) {
+
+	Scanner scanner = new Scanner(System.in);
+	int[] arr = { 2, 4 };
+	System.out.println("nhập số bất kỳ");
+	int ketqua = arr[0]+ arr[1];
+	System.out.println("ket qua la" + ketqua);		 
+}}
