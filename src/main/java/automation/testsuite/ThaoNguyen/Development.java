3a5fb60f641c9a485b562f36c5d299dddb1e4336
unknown <thaontt94nd@gmail.com>
2023-06-27 22:33:57 +0700

    developmet1

--- src/main/java/automation/testsuite/ThaoNguyen/Development.java
+++ src/main/java/automation/testsuite/ThaoNguyen/Development.java
@@ -3,10 +3,10 @@ package automation.testsuite.ThaoNguyen;
 public class Development extends Employee {
 	public int salary;
 	@Override
-	public void salary() {
-		// TODO Auto-generated constructor stub
-		salary = super.getBasic_salary() + (super.getBasic_salary()*2)+(super.getOvertime()*((super.getBasic_salary() + (super.getBasic_salary()*2)/22)/8)) ;
+	public int salary() {
+		return salary = super.getBasic_salary() + (super.getBasic_salary()*2)+(super.getOvertime()*((super.getBasic_salary() + (super.getBasic_salary()*2)/22)/8));
 	}
+	
 	@Override
 	public String print() {
 		return super.print() + ", Salary: " + this.salary;
