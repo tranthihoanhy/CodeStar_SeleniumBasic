5f0def104d0a99bdd39eb96e77318b42785c8011
unknown <thaontt94nd@gmail.com>
2023-06-27 22:32:43 +0700

    employee1

--- src/main/java/automation/testsuite/ThaoNguyen/Employee.java
+++ src/main/java/automation/testsuite/ThaoNguyen/Employee.java
@@ -1,8 +1,18 @@
 package automation.testsuite.ThaoNguyen;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Employee {
-	private String name, phone, email;
+	private String name, phone, email,department;
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 	private int id, age, overtime, Basic_salary;
 	
 	public String getName() {
@@ -64,22 +74,26 @@ public abstract class Employee {
 	public void inputData() {
 		System.out.print("name:");
 		name = sc.nextLine();
-		System.out.print("phone:");
-		phone = sc.nextLine();
-		System.out.print("email:");
-		email = sc.nextLine();
+//		System.out.print("phone:");
+//		phone = sc.nextLine();
+//		System.out.print("email:");
+//		email = sc.nextLine();
+		System.out.print("department:");
+		department = sc.nextLine();
 		System.out.print("id:");
 		id = sc.nextInt();
-		System.out.print("age:");
-		age = sc.nextInt();
+//		System.out.print("age:");
+//		age = sc.nextInt();
 		System.out.print("Basic_salary:");
 		Basic_salary = sc.nextInt();
 		System.out.print("overtime:");
 		overtime = sc.nextInt();
 	}
 	public String print() {
-		return "ID: "+ this.id + ",name: "+ this.name+  ",age: "+ this.age + ".phone: "+ this.phone+ ".email: "+ this.email; 
+		return "ID: "+ this.id + ",name: "+ this.name
+//				+  ",age: "+ this.age + ".phone: "+ this.phone+ ".email: "+ this.email
+				+".Department: "+ this.department ; 
 	}
 	
-	public abstract void salary();
+	public abstract int salary();
 }
