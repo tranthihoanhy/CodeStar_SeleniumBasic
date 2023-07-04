82b0f6605e851bedb4efcd8a899dc9d4cc5cba66
duongthangictu <54371511+duongthangictu@users.noreply.github.com>
2023-07-05 00:45:22 +0700

    Add files via upload

---
+++ src/main/java/automation/common/Day13_CT_Account_ThangDuong.java
@@ -0,0 +1,5 @@
+package automation.common;
+
+public class Day13_CT_Account_ThangDuong {
+	public static String webURL = "https://rise.fairsketch.com/signin?redirect=https://rise.fairsketch.com/events";
+}
